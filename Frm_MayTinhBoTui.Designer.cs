fd78aec7fcf2f87a4abff19d626cdeeab67a0e00
Curtttt <95266553+Curtttt@users.noreply.github.com>
2023-08-29 03:24:21 +0700

    Add files via upload

---
+++ Frm_MayTinhBoTui.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WinForm
+{
+    partial class Frm_MayTinhBoTui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            buttonC = new Button();
+            button_kq = new Button();
+            button_cong = new Button();
+            button_tru = new Button();
+            button_nhan = new Button();
+            button_chia = new Button();
+            label = new Label();
+            txt = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Thistle;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(107, 154);
+            button1.Name = "button1";
+            button1.Size = new Size(71, 48);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += num_click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Thistle;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(227, 154);
+            button2.Name = "button2";
+            button2.Size = new Size(71, 48);
+            button2.TabIndex = 0;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += num_click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Thistle;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(346, 154);
+            button3.Name = "button3";
+            button3.Size = new Size(71, 48);
+            button3.TabIndex = 0;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += num_click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Thistle;
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(466, 154);
+            button4.Name = "button4";
+            button4.Size = new Size(71, 48);
+            button4.TabIndex = 0;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += num_click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Thistle;
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(107, 227);
+            button5.Name = "button5";
+            button5.Size = new Size(71, 48);
+            button5.TabIndex = 0;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += num_click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Thistle;
+            button6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(227, 227);
+            button6.Name = "button6";
+            button6.Size = new Size(71, 48);
+            button6.TabIndex = 0;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += num_click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Thistle;
+            button7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(346, 227);
+            button7.Name = "button7";
+            button7.Size = new Size(71, 48);
+            button7.TabIndex = 0;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += num_click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Thistle;
+            button8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(466, 227);
+            button8.Name = "button8";
+            button8.Size = new Size(71, 48);
+            button8.TabIndex = 0;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += num_click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Thistle;
+            button9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(107, 297);
+            button9.Name = "button9";
+            button9.Size = new Size(71, 48);
+            button9.TabIndex = 0;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += num_click;
+            // 
+            // button0
+            // 
+            button0.BackColor = Color.Thistle;
+            button0.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button0.Location = new Point(227, 297);
+            button0.Name = "button0";
+            button0.Size = new Size(71, 48);
+            button0.TabIndex = 0;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = false;
+            button0.Click += num_click;
+            // 
+            // buttonC
+            // 
+            buttonC.BackColor = Color.Thistle;
+            buttonC.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonC.Location = new Point(346, 297);
+            buttonC.Name = "buttonC";
+            buttonC.Size = new Size(71, 48);
+            buttonC.TabIndex = 0;
+            buttonC.Text = "C";
+            buttonC.UseVisualStyleBackColor = false;
+            buttonC.Click += del;
+            // 
+            // button_kq
+            // 
+            button_kq.BackColor = Color.Thistle;
+            button_kq.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button_kq.Location = new Point(466, 297);
+            button_kq.Name = "button_kq";
+            button_kq.Size = new Size(71, 48);
+            button_kq.TabIndex = 0;
+            button_kq.Text = "=";
+            button_kq.UseVisualStyleBackColor = false;
+            button_kq.Click += show;
+            // 
+            // button_cong
+            // 
+            button_cong.BackColor = Color.Thistle;
+            button_cong.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button_cong.Location = new Point(107, 367);
+            button_cong.Name = "button_cong";
+            button_cong.Size = new Size(71, 48);
+            button_cong.TabIndex = 0;
+            button_cong.Text = "+";
+            button_cong.UseVisualStyleBackColor = false;
+            button_cong.Click += tinh;
+            // 
+            // button_tru
+            // 
+            button_tru.BackColor = Color.Thistle;
+            button_tru.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button_tru.Location = new Point(227, 367);
+            button_tru.Name = "button_tru";
+            button_tru.Size = new Size(71, 48);
+            button_tru.TabIndex = 0;
+            button_tru.Text = "-";
+            button_tru.UseVisualStyleBackColor = false;
+            button_tru.Click += tinh;
+            // 
+            // button_nhan
+            // 
+            button_nhan.BackColor = Color.Thistle;
+            button_nhan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button_nhan.Location = new Point(346, 367);
+            button_nhan.Name = "button_nhan";
+            button_nhan.Size = new Size(71, 48);
+            button_nhan.TabIndex = 0;
+            button_nhan.Text = "x";
+            button_nhan.UseVisualStyleBackColor = false;
+            button_nhan.Click += tinh;
+            // 
+            // button_chia
+            // 
+            button_chia.BackColor = Color.Thistle;
+            button_chia.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button_chia.Location = new Point(466, 367);
+            button_chia.Name = "button_chia";
+            button_chia.Size = new Size(71, 48);
+            button_chia.TabIndex = 0;
+            button_chia.Text = "/";
+            button_chia.UseVisualStyleBackColor = false;
+            button_chia.Click += tinh;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Font = new Font("Segoe UI Black", 30F, FontStyle.Bold, GraphicsUnit.Point);
+            label.ForeColor = Color.MediumSlateBlue;
+            label.Location = new Point(85, 9);
+            label.Name = "label";
+            label.Size = new Size(475, 67);
+            label.TabIndex = 1;
+            label.Text = "MÁY TÍNH BỎ TÚI";
+            // 
+            // txt
+            // 
+            txt.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txt.Location = new Point(85, 98);
+            txt.Name = "txt";
+            txt.Size = new Size(475, 34);
+            txt.TabIndex = 2;
+            txt.TextAlign = HorizontalAlignment.Right;
+            // 
+            // Frm_MayTinhBoTui
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(652, 450);
+            Controls.Add(txt);
+            Controls.Add(label);
+            Controls.Add(button_chia);
+            Controls.Add(button_kq);
+            Controls.Add(button8);
+            Controls.Add(button_nhan);
+            Controls.Add(button4);
+            Controls.Add(buttonC);
+            Controls.Add(button7);
+            Controls.Add(button_tru);
+            Controls.Add(button3);
+            Controls.Add(button0);
+            Controls.Add(button6);
+            Controls.Add(button_cong);
+            Controls.Add(button2);
+            Controls.Add(button9);
+            Controls.Add(button5);
+            Controls.Add(button1);
+            Name = "Frm_MayTinhBoTui";
+            Text = "Máy tính bỏ túi";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button buttonC;
+        private Button button_kq;
+        private Button button_cong;
+        private Button button_tru;
+        private Button button_nhan;
+        private Button button_chia;
+        private Label label;
+        private TextBox txt;
+    }
+}
---
+++ Frm_MayTinhBoTui.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class Frm_MayTinhBoTui : Form
+    {
+        public Frm_MayTinhBoTui()
+        {
+            InitializeComponent();
+        }
+
+        string phep;
+        private void num_click(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+
+            txt.Text += num.Text;
+        }
+
+        private void del(object sender, EventArgs e)
+        {
+            txt.Clear();
+        }
+
+        private void tinh(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            phep = btn.Text;
+            txt.Text += " " + phep + " ";
+        }
+
+        private void show(object sender, EventArgs e)
+        {
+            string[] lst = txt.Text.Split(' ');
+
+            switch (lst[1])
+            {
+                case "+":
+                    txt.Text = (int.Parse(lst[0]) + int.Parse(lst[2])).ToString();
+                    break;
+                case "-":
+                    txt.Text = (int.Parse(lst[0]) - int.Parse(lst[2])).ToString();
+                    break;
+                case "x":
+                    txt.Text = (int.Parse(lst[0]) * int.Parse(lst[2])).ToString();
+                    break;
+                case "/":
+                    txt.Text = (float.Parse(lst[0]) / float.Parse(lst[2])).ToString();
+                    break;
+            }
+        }
+    }
+}
